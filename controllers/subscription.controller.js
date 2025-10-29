4f0e0b6e879a795a7c607861a41d7110fc92e1b2
clemdevlin <egyayemi2006@gmail.com>
2025-10-29 15:08:04 +0000

    added subscriptions delete

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,6 +1,6 @@
-import Subscription from '../models/subscription.model.js'
-import { workflowClient } from '../config/upstash.js'
-import { SERVER_URL } from '../config/env.js'
+import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -15,22 +15,24 @@ export const createSubscription = async (req, res, next) => {
         subscriptionId: subscription.id,
       },
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       retries: 0,
-    })
+    });
 
-    res.status(201).json({ success: true, data: { subscription, workflowRunId } });
+    res
+      .status(201)
+      .json({ success: true, data: { subscription, workflowRunId } });
   } catch (e) {
     next(e);
   }
-}
+};
 
 export const getUserSubscriptions = async (req, res, next) => {
   try {
     // Check if the user is the same as the one in the token
-    if(req.user.id !== req.params.id) {
-      const error = new Error('You are not the owner of this account');
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this account");
       error.status = 401;
       throw error;
     }
@@ -41,4 +43,45 @@ export const getUserSubscriptions = async (req, res, next) => {
   } catch (e) {
     next(e);
   }
-}
+};
+
+export const deleteSubscription = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    // Find the subscription
+    const subscription = await Subscription.findById({ _id: id });
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+
+    // Ensure the user owns this subscription
+    if (subscription.user.toString() !== req.user.id) {
+      const error = new Error(
+        "You are not authorized to delete this subscription"
+      );
+      error.status = 403;
+      throw error;
+    }
+
+    // (Optional) Cancel any associated workflow or reminder
+    try {
+      // Uncomment and modify if you want to cancel related workflows in Upstash
+      // await workflowClient.cancel({ workflowRunId: subscription.workflowRunId });
+    } catch (workflowError) {
+      console.warn("Failed to cancel Upstash workflow:", workflowError.message);
+    }
+
+    await subscription.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Subscription deleted successfully",
+    });
+  } catch (e) {
+    next(e);
+  }
+};
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,26 +1,37 @@
-import { Router } from 'express';
-import authorize from '../middlewares/auth.middleware.js'
+import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
 import {
   createSubscription,
+  deleteSubscription,
   getUserSubscriptions,
-} from '../controllers/subscription.controller.js'
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }));
+subscriptionRouter.get("/", (req, res) =>
+  res.send({ title: "GET all subscriptions" })
+);
 
-subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }));
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: "GET subscription details" })
+);
 
-subscriptionRouter.post('/', authorize, createSubscription);
+subscriptionRouter.post("/", authorize, createSubscription);
 
-subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
+subscriptionRouter.put("/:id", (req, res) =>
+  res.send({ title: "UPDATE subscription" })
+);
 
-subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
+subscriptionRouter.delete("/:id", authorize, deleteSubscription);
 
-subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 
-subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }));
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: "CANCEL subscription" })
+);
 
-subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({ title: 'GET upcoming renewals' }));
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ title: "GET upcoming renewals" })
+);
 
 export default subscriptionRouter;
